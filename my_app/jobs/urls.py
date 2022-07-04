d59cdec5fe0b0f350276bd736b5d0ce85c73fb70
mesujitg <mesujitg@gmail.com>
2022-07-04 11:42:58 +0545

    data selection

--- my_app/jobs/urls.py
+++ my_app/jobs/urls.py
@@ -4,7 +4,7 @@ from jobs import views
 
 urlpatterns = [
     path('', views.show_jobs),
-    path('single', views.show_single_job),
+    path('single/<id>', views.show_single_job),
     path('categories', views.show_job_by_category),
     path('search', views.show_searched_jobs),
 ]
--- my_app/jobs/views.py
+++ my_app/jobs/views.py
@@ -1,12 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from jobs.models import Job
 
 
 def show_jobs(request):
-    return render(request, 'jobs.html')
+    jobs = Job.objects.all()
+    return render(request, 'jobs.html', {'jobs': jobs})
 
 
-def show_single_job(request):
-    pass
+def show_single_job(request, id):
+    # job = Job.objects.filter(id=id)
+    job = Job.objects.get(id=id)
+    return render(request, 'job_details.html', {'job': job})
 
 
 def show_job_by_category(request):
