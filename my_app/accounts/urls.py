0ea26c906f7dad85ffee7f72db82c011cd1969cd
mesujitg <mesujitg@gmail.com>
2022-07-11 12:09:04 +0545

    django user module/registration

--- my_app/accounts/urls.py
+++ my_app/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from accounts import views
 
 urlpatterns = [
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('register_user', views.register_user, name='register_user'),
 ]
--- my_app/accounts/views.py
+++ my_app/accounts/views.py
@@ -1,5 +1,32 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from jobseekers.models import JobSeeker
 
 
 def login(request):
     return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        fn = request.POST['fname']
+        ln = request.POST['lname']
+        em = request.POST['email']
+        un = request.POST['username']
+        pw = request.POST['password']
+
+        user = User(first_name=fn, last_name=ln, email=em, username=un, password=pw)
+        user.save()
+
+        js = JobSeeker(user=user, objective='', qualification='', training='', skills='',
+                       experience='', cv='', image='')
+        js.save()
+
+        return HttpResponse('Ok')
+
+    return HttpResponse('Invalid Access')
